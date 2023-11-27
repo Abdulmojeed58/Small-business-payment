d72c27ecf35f339786ecd750eb0f787e7848a8c3
Abdulmojeed <mojeedayoola58@gmail.com>
2023-11-27 13:13:25 +0100

    build

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,7 +1,4 @@
-// @ts-ignore  
-// import Signup from "../../components/Register";
-
-import Signup from "@/components/Register";
+import Signup from "../../components/Register";
 
 const RegisterPage = () => {
   return <Signup />;
